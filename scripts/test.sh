81ccadd967761f92289b0ef128fdf3a90d957735
SpandanK87 <spandan.k87@gmail.com>
2025-02-25 19:18:12 +0530

    secrets

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,21 +8,21 @@ else
 fi
 
 # Check if repository details are provided as environment variables
-if [ -z "$GITHUB_REPOSITORY" ]; then
+if [ -z "${{ secrets.GH_REPOSITORY }}" ]; then
     echo "Error: GITHUB_REPOSITORY environment variable is not set"
     echo "Please set it in .env file or use: GITHUB_REPOSITORY=owner/repo $0"
     exit 1
 fi
 
 # Extract owner and repo from GITHUB_REPOSITORY
-OWNER=$(echo $GITHUB_REPOSITORY | cut -d'/' -f1)
-REPO=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
+OWNER=$(echo ${{ secrets.GH_REPOSITORY }} | cut -d'/' -f1)
+REPO=$(echo ${{ secrets.GH_REPOSITORY }} | cut -d'/' -f2)
 
 # GitHub API URL
 API_URL="https://api.github.com/repos/$OWNER/$REPO/commits"
 
 # Fetch all commits
-echo "Fetching commits from $GITHUB_REPOSITORY..."
+echo "Fetching commits from ${{ secrets.GH_REPOSITORY }}..."
 curl -s "$API_URL" | \
     jq -r '.[] | "Commit: \(.sha)\nAuthor: \(.commit.author.name)\nDate: \(.commit.author.date)\nMessage: \(.commit.message)\n--------------------------------"'
 
